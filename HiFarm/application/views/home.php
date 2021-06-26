3151df1953de624d7d52b7e29594657e04882d69
Dri-anS <rianystn@gmail.com>
2021-06-26 11:06:53 +0700

    fix error logo hifarn

--- HiFarm/application/views/home.php
+++ HiFarm/application/views/home.php
@@ -21,7 +21,7 @@
 
 <body>
     <nav class="navbar navbar-light navbar-expand-md navigation-clean" style="background: var(--teal);">
-        <div class="container"><a class="navbar-brand" href="href="<?= base_url()."index.php/main_page/home";?> style="color: var(--white);">HiFarm</a>
+    <div class="container"><a class="navbar-brand" href="<?php echo base_url();""?>"style="color: var(--white);">HiFarm</a>
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="<?= base_url()."index.php/main_page/ask_2"; ?>" style="color: var(--white);">Ask</a></li>
                 <li class="nav-item"><a class="nav-link" href="<?= base_url()."index.php/main_page/discussion"; ?>" style="color: var(--white);">Discussion</a></li>
